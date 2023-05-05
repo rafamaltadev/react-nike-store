f2b554dd6441cb2f6530a717b19835ae1fe5716f
Rafa Malta <rafael.projetos82@gmail.com>
2023-05-05 11:10:42 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import {
+  Cart,
+  FlexContent,
+  Footer,
+  Hero,
+  Navbar,
+  Sales,
+  Stories,
+} from "./components";
+import {
+  heroapi,
+  popularsales,
+  topratesales,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data.js";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Cart />
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={topratesales} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
+    </>
+  );
+};
+
+export default App;
